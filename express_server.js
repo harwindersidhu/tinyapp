083189bd958e917d066b63386f617e10c9c544c9
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-05-07 14:49:07 -0600

    Updated express_server.js to work with helpers.js file

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const bcrypt = require("bcryptjs");
 let cookieSession = require('cookie-session');
 app.set('trust proxy', 1) // trust first proxy
 
+const { getUserByEmail, generateRandomString, urlsForUser, urlBelongsToCurrentUser } = require("./helpers");
+
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
@@ -41,7 +43,7 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    urls: urlsForUser(req.session.user_id), 
+    urls: urlsForUser(req.session.user_id, urlDatabase), 
     currentUser: users[req.session.user_id], 
   };
   res.render("urls_index", templateVars);
@@ -61,7 +63,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let longURL = "";
-  if (urlBelongsToCurrentUser(req.params.shortURL, req.session.user_id)) { 
+  if (urlBelongsToCurrentUser(req.params.shortURL, req.session.user_id, urlDatabase)) { 
     longURL = urlDatabase[req.params.shortURL].longURL;
   } else {
     longURL = null;
@@ -104,7 +106,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (!(users[req.session.user_id])) { 
     res.status(403).send("You must log in to delete.");
-  } else if (!(urlBelongsToCurrentUser(req.params.shortURL, req.session.user_id))) { 
+  } else if (!(urlBelongsToCurrentUser(req.params.shortURL, req.session.user_id, urlDatabase))) { 
     res.status(403).send("This url doesnot belong to you. So you can not delete it.");
   } else {
     delete urlDatabase[req.params.shortURL];
@@ -115,7 +117,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   if (!(users[req.session.user_id])) { 
     res.status(403).send("You must log in to update.");
-  } else if (!(urlBelongsToCurrentUser(req.params.id, req.session.user_id))) { 
+  } else if (!(urlBelongsToCurrentUser(req.params.id, req.session.user_id, urlDatabase))) { 
     res.status(403).send("This url doesnot belong to you. So you can not update it.");
   } else {
     let newLongURL = req.body.newLongURL;
@@ -128,12 +130,12 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  let currUserId = userIsExisting(email);
+  let currUserId = getUserByEmail(email, users);
   if (!email && !password) {
     res.status(400).send("Email or password can't be empty");
   } else if (!currUserId) {
     res.status(403).send("Email cannot be found. Please check your email.");
-  } else if (!(passwordMatches(currUserId, password))) {
+  } else if (!(bcrypt.compareSync(password, users[currUserId].password))) { 
     res.status(403).send("Wrong password. Please check your password.");
   } else {
     req.session.user_id = currUserId;
@@ -168,7 +170,7 @@ app.post("/register", (req, res) => {
   //If email or password is empty string, send back a response with the 400 status code
   if (email === "" || password === "") {
     res.status(400).send("Email or password can't be empty");
-  } else if (userIsExisting(email)) { //If email is already existing, send back a response with the 400 status code
+  } else if (getUserByEmail(email, users)) { //If email is already existing, send back a response with the 400 status code
     res.status(400).send("Email already exists. Please use another email");
   } else {
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -183,75 +185,6 @@ app.post("/register", (req, res) => {
     res.redirect("/urls");
   } 
 });
-
-/**
- * @returns a string of 6 random alphanumeric characters
- */
-const generateRandomString = () => {
-  let characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let result = "";
-  let charactersLength = characters.length;
-
-  for (let i = 0; i <= 5; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-
-  return result;
-};
-
-/**
- * 
- * @param {*} email The email we needed to check if it is existing or not
- * @returns userId if email exists in users object, else returns null
- */
-const userIsExisting = (email) => {
-  for (let userId in users) {
-    if (users[userId].email === email) {
-      return userId;
-    }
-  }
-  return null;
-};
-
-/**
- * 
- * @param userId userId of user
- * @param pass password received through request
- * @returns true if pass matches the password of saved user, else return false
- */
-const passwordMatches = (userId, pass) => {
-  return bcrypt.compareSync(pass, users[userId].password);
-}
-
-/**
- * 
- * @param id id of the currently logged in user
- * @returns only that objects of urlDatabase which are relevent to currently logged in user
- */
-const urlsForUser = (id) => {
-  let currUserUrlDatabase = {};
-  for (let url in urlDatabase) {
-    if (urlDatabase[url].userID === id) {
-      currUserUrlDatabase[url] = urlDatabase[url];
-    }
-  }
-  return currUserUrlDatabase;
-};
-
-/**
- * 
- * @param url shortUrl to check if it belongs to current user
- * @param currUserId userID of current user
- * @returns true if provided short url belongs to current user, else returns false.
- */
-const urlBelongsToCurrentUser = (url, currUserId) => {
-  let currUserUrlDatabase = urlsForUser(currUserId);
-  let shortUrlsForCurrUser = Object.keys(currUserUrlDatabase);
-  if (!(shortUrlsForCurrUser.includes(url))) {
-    return false;
-  } 
-  return true;
-}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
